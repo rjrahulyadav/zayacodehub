8cc23373bbf6e54aab31ab2a2355ceefba611cff
ZAYA CODE HUB <zayacodehub@gmail.com>
2026-01-28 10:24:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3019505295-8da19",
+  "appId": "1:686270253343:web:8ec95bba3e828add62882a",
+  "apiKey": "AIzaSyD8sKPo3DpdHGWBzT3C2rwi3jBLvaxBX0I",
+  "authDomain": "studio-3019505295-8da19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "686270253343"
+};
